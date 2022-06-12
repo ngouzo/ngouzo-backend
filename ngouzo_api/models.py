8bca3483fdc226a0871827fa1e6839372a9b8517
seggi SMS <seggimarugira@gmail.com>
2022-06-12 12:00:41 +0200

    add some models

--- ngouzo_api/models.py
+++ ngouzo_api/models.py
@@ -14,6 +14,12 @@ class SchoolClasses(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
 
+class FeeType(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
 class Currencies(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
     code = models.CharField(max_length=10, blank=True, default='')
@@ -86,6 +92,11 @@ class Modules(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
 
+# Default
+'''
+Transport,
+Fee
+'''
 # Payment Categories
 
 
@@ -97,8 +108,42 @@ class PaymentCategories(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     deleted = models.DateTimeField(auto_now_add=True)
-# Students  sections
 
+# To be filled by dean of students
+
+
+class ModuleToClass(models.Model):
+    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(SchoolClasses, on_delete=models.CASCADE)
+    module_id = models.ForeignKey(Modules, on_delete=models.CASCADE)
+    grades = models.FloatField(blank=True)
+    hours = models.CharField(blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    deleted = models.DateTimeField(auto_now_add=True)
+
+# Teacher section
+
+
+class TeacherClass(models.Model):
+    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    teach_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(SchoolClasses, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+# To be managed by  dean of students
+
+
+class TeacherClassModule(models.Model):
+    teach_class_id = models.ForeignKey(TeacherClass, on_delete=models.CASCADE)
+    module_id = models.ForeignKey(ModuleToClass, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+# Students  sections
+# To be used by secretary account
 
 class Students(models.Model):
     school_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -115,6 +160,17 @@ class Students(models.Model):
     updated = models.DateTimeField(auto_now_add=True)
 
 
+class StudentsAddress(models.Model):
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    country = models.CharField(max_length=100, blank=True, default='')
+    state = models.CharField(max_length=100, blank=True, default='')
+    city = models.CharField(max_length=100, blank=True, default='')
+    street = models.CharField(max_length=100, blank=True, default='')
+    building = models.CharField(max_length=100, blank=True, default='')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
 class StudentRelative(models.Model):
     student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
     father_fullname = models.CharField(max_length=100, blank=True, default='')
@@ -126,38 +182,107 @@ class StudentRelative(models.Model):
 class StudentLevel(models.Model):
     student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
     current_level = models.BooleanField(blank=True, default=False)
-    studding_year = models.CharField(max_length=20, blank=True, default='')
+    studding_range_year = models.CharField(
+        max_length=20, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
+
+# To be used by cashier
 
 
 class StudentPayment(models.Model):
     student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
     payment_category = models.ForeignKey(
         PaymentCategories, on_delete=models.CASCADE)
+    fee_type = models.ForeignKey(FeeType, on_delete=models.CASCADE)
+    description = models.CharField(default='', blank=True)
     amount_paid = models.FloatField()
     currency = models.ForeignKey(Currencies, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
 
-class ModuleToClass(models.Model):
-    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    class_id = models.ForeignKey(SchoolClasses, on_delete=models.CASCADE)
-    module_id = models.ForeignKey(Modules, on_delete=models.CASCADE)
+# Default grade
+'''
+Fail,
+Pass
+'''
+
+# To be used by a  teacher
 
 
-# Teacher section
-class TeacherClass(models.Model):
-    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    class_id = models.ForeignKey(SchoolClasses, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
-
-
-class TeacherClassModule(models.Model):
-    teach_class_id = models.ForeignKey(TeacherClass, on_delete=models.CASCADE)
+class StudentMarksEvaluation(models.Model):
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    teach_id = models.ForeignKey(User, on_delete=models.CASCADE)
     module_id = models.ForeignKey(ModuleToClass, on_delete=models.CASCADE)
+    student_score = models.FloatField(blank=True)
+    student_grade = models.FloatField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
+
+
+# To be used Accounting
+# Default
+'''
+    Salary,
+    Surplus
+'''
+
+
+class EmployeeFinancial(models.Model):
+    teach_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField(blank=True, default='')
+    type = models.CharField(blank=True, default='')
+    currency = models.ForeignKey(Currencies, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+# Default
+'''
+    Advance salary
+    salary
+    Credit,
+    bonus
+'''
+'''
+Month Example
+JANUARY
+'''
+
+
+class EmployeePaymentHistory(models.Model):
+    teach_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField(blank=True, default='')
+    month = models.CharField(default='', blank=True)
+    year = models.CharField(default='', blank=True)
+    payment_type = models.CharField(blank=True, default='')
+    description = models.CharField(default='', blank=True)
+    currency = models.ForeignKey(Currencies, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+class StudentBusSubscription(models.Model):
+    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+# To be used by cashier
+
+
+class StudentBusSubscription(models.Model):
+    school_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+class StudentBusAttendance(models.Model):
+    student_subscription_id = models.ForeignKey(
+        StudentBusSubscription, on_delete=models.CASCADE)
+    pick_student = models.BooleanField(default=False)
+    arrive_school = models.BooleanField(default=False)
+    picked_at = models.DateTimeField()
+    arrived_at = models.DateTimeField()
