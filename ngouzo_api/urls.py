7a0b86f2f6e44844992b2a6ae3e4d9a9eae237f3
seggi SMS <seggimarugira@gmail.com>
2022-07-11 20:53:54 +0200

    add some change

--- ngouzo_api/urls.py
+++ ngouzo_api/urls.py
@@ -3,11 +3,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+from .ng_views.ngObtainTokenPair import NGObtainTokenPairView
 from .views import REGISTER_VIEW, LOGIN_VIEW
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login/', NGObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', REGISTER_VIEW.as_view(), name='auth_register'),
-    path('login/', LOGIN_VIEW, name="auth_login"),
+
 ]
