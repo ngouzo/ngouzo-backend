63ef73303342fdb82b8289a508595d29ed128e55
seggi SMS <seggimarugira@gmail.com>
2022-07-11 23:54:32 +0200

    add some changes

---
+++ infrastructure/env.go
@@ -0,0 +1,14 @@
+package infrastructure
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Unable to load .env file")
+	}
+}
